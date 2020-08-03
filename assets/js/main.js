8a8155fd27c2fa0521248171de32ed48ee448107
Adam Susuł <a.w.susul@gmail.com>
2020-08-03 20:04:30 +0200

    Add PanControl - plugin

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+let mymap;
+let lyrOSM;
+let mrkCurrentLocation;
+let panControl;
+
+mymap = L.map('mapdiv', { center: [52.22758, 16.82593], zoom: 13 });
+
+//link to the map preview
+lyrOSM = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+//displaying the map in the browser
+mymap.addLayer(lyrOSM);
+
+
+// keypress 'l' = my location
+mymap.on('keypress', function (e) {
+    if (e.originalEvent.key == "l") {
+        mymap.locate();
+    }
+});
+
+mymap.on('locationfound', function (e) {
+    mrkCurrentLocation = L.circleMarker(e.latlng).addTo(mymap);
+    mymap.setView(e.latlng, 12);
+});
+
+mymap.on('locationerror', function (e) {
+    console.log(e);
+    alert("Location was not found");
+});
+
+panControl = L.control.pan().addTo(mymap);
+
+
